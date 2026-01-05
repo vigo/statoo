04e0947fe6d07bac28ecc33a12a149d54e49a9a6
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2026-01-05 17:47:09 +0300

    Bump version: 2.0.3 → 2.1.0

--- app/version/version.go
+++ app/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is the current version of statoo.
-const Version string = "2.0.3"
+const Version string = "2.1.0"
 
 // CommitHash represents current build/commit hash.
 var CommitHash string
